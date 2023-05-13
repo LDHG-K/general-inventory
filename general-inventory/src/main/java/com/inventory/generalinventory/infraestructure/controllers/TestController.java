c64b14faebf0ae1adb8fc87146b178aa8f218b12
Luis Herrera <luisdavidherrerag@gmail.com>
2023-05-13 03:34:10 -0500

    Docker test 3

--- general-inventory/src/main/java/com/inventory/generalinventory/infraestructure/controllers/TestController.java
+++ general-inventory/src/main/java/com/inventory/generalinventory/infraestructure/controllers/TestController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World bellaca!";
+        System.out.println(LocalDate.now());
+        return "Hello World ;)!";
     }
 }
