9efb864e7a010ca6419f8d878cc7ec577af206ac
Luis Herrera <luisdavidherrerag@gmail.com>
2023-05-13 01:17:41 -0500

    Proyect Config

---
+++ general-inventory/src/main/java/com/inventory/generalinventory/GeneralInventoryApplication.java
@@ -0,0 +1,15 @@
+package com.inventory.generalinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralInventoryApplication.class, args);
+		System.out.println("Inventory Application Started :) ");
+	}
+
+
+}
